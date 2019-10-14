af1a47b54e6d7edb828f056e5a11e484d62fe2eb
lluisllcp <CicloM@192.168.2.225>
2019-10-14 09:56:30 +0200

    :tada: Commit Inicial

---
+++ src/org/japo/java/main/Main.java
@@ -0,0 +1,47 @@
+/* 
+ * Copyright 2019 Lluís CP - aw.lluiscerda@gmail.com.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.japo.java.main;
+
+import java.util.Locale;
+import java.util.Random;
+/**
+ *
+ * @author Lluís CP - aw.lluiscerda@gmail.com
+ */
+public class Main {
+    
+     // Instanciar Random
+    public static final Random RND = new Random();
+    
+    
+    public static void main(String[] args) {
+      
+         // Constantes
+        final double ALTURA_MIN = 0.50;
+        final double ALTURA_MAX = 2.50;
+
+        // Variable
+        double altura;
+
+        // Generar dato
+        altura = RND.nextDouble() * (ALTURA_MAX - ALTURA_MIN) + ALTURA_MIN;
+
+        // Mensaje
+        System.out.printf(Locale.ENGLISH,
+                "Altura aleatoria ..........: %.2f metros%n", altura);
+    }
+}
+ 
